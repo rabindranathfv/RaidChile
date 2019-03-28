f76efeb0973b56f6796da8b167c3a34fb0d9d398
Rafael V <ravb2013@gmail.com>
2019-03-28 15:53:55 -0400

    Add mail list subscription django form
    - Adding the mail list subscription django form to raidchileapp.
    - Displaying the previous form with correct styles in the home.html template.
    - Making the textarea widget of the contact form unresizable.

--- raidchileapp/forms.py
+++ raidchileapp/forms.py
@@ -6,6 +6,13 @@ from django.core.validators import RegexValidator
 class SearchForm(forms.Form):
 	your_name = forms.CharField(label='Your name', max_length=100)
 
+class MailListForm(forms.Form):
+	email = forms.EmailField(	label='Email',
+								max_length=100,
+								widget=forms.EmailInput(attrs={'placeholder': 'Su dirección de email',
+																'class': 'w3-input w3-border'})
+								)
+
 class ContactForm(forms.Form):
 	name = forms.CharField(	label='Nombre',
 							max_length=100,
@@ -33,5 +40,5 @@ class ContactForm(forms.Form):
 								widget=forms.Textarea(attrs={'placeholder': 'Mensaje',
 															'rows': 3,
 															'cols': 50,
-															'class': 'w3-input w3-padding-16 w3-border'})
+															'class': 'w3-input w3-padding-16 w3-border no-resize'})
 								)
--- raidchileapp/views.py
+++ raidchileapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from .forms import ContactForm
+from .forms import ContactForm, MailListForm
 
 # Create your views here.
 def home(request):
 	contact_form = ContactForm()
-	context = {'contact_form': contact_form }
+	mail_form = MailListForm()
+	context = {'contact_form': contact_form, 'mail_form': mail_form }
 	return render(request, "raidchileapp/home.html", context)
