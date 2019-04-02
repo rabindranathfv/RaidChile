72dedcd8cf60961d7a17f575528bab361c7f8d15
Rafael V <ravb2013@gmail.com>
2019-04-02 09:36:29 -0400

    Add responsive behaviour to filters sidebar
    - Adding a script and responsive classes to the filters sidebar of tour
    search template.
    - Adding filter icon to sidebar.

--- raidchileapp/static/js/behaviour.js
+++ raidchileapp/static/js/behaviour.js
@@ -1,4 +1,10 @@
 $( document ).ready(function(){
 	// Guarantee that the datepicker uses the correct dateFormat and minDate.
 	$(".datepicker.reservation").datepicker({ dateFormat: 'dd/mm/yy', minDate: 0});
+
+	// Search filter sidebar responsive functions
+	$("#filter-toggle").click(function(){
+		console.log("Click");
+		$("#filters-sidebar").toggleClass("w3-hide-small");
+	});
 });
