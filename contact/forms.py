0e57982c9817bdf43baaec235c63348ef765fcc9
Rafael V <ravb2013@gmail.com>
2019-04-07 12:03:15 -0400

    Create ContactMessage model, migration and  ModelForm
    - Creating a new ContactMessage model to track all the user sent contact
    messages in Django Admin.
    - Created a migration for the new model.
    - Creating a ModelForm from Contact Message to replace the old contact form
    in the future.

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,56 @@
 from django import forms
 from django.core.validators import RegexValidator
 
+from .models import ContactMessage
+
+class ContactForm2(forms.ModelForm):
+	
+	phone_number = forms.CharField(
+		label='Phone',
+		max_length=100,
+		validators=[RegexValidator(
+			'^(\+*\d+)+$',
+			message="Numbers without spaces only."
+		)],
+		widget=forms.TextInput(
+			attrs={
+				'placeholder': 'Phone: e.g.+0123456789',
+				'class': 'w3-input w3-padding-16 w3-border'
+			}
+		)
+	)
+
+	class Meta:
+		model = ContactMessage
+		fields = [
+			'full_name',
+			'email',
+			'phone',
+			'message'
+		]
+		widgets = {
+			'full_name': forms.TextInput(
+				attrs={
+					'placeholder': 'Full name',
+					'class': 'w3-input w3-padding-16 w3-border'
+				}
+			),
+			'email': forms.EmailInput(
+				attrs={
+					'placeholder': 'Email',
+					'class': 'w3-input w3-padding-16 w3-border'
+				}
+			),
+			'message': forms.Textarea(
+				attrs={
+					'placeholder': 'Message',
+					'rows': 3,
+					'cols': 50,
+					'class': 'w3-input w3-padding-16 w3-border no-resize'
+				}
+			)
+		}
+
 class ContactForm(forms.Form):
 	name = forms.CharField(
 		label='Full name',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class ContactMessage(models.Model):
+	full_name = models.CharField(
+		max_length=150,
+		db_index=True,
+		verbose_name='full name'
+	)
+	email = models.EmailField(
+		max_length=100,
+		verbose_name='email'
+	)
+	phone = models.CharField(
+		max_length=30,
+		blank=True,
+		verbose_name='phone'
+	)
+	message = models.TextField(
+		max_length=500,
+		verbose_name='message'
+	)
+	created_at = models.DateTimeField(
+		auto_now_add=True,
+		verbose_name='created at'
+	)
+	updated_at = models.DateTimeField(
+		auto_now=True,
+		verbose_name='updated at'
+	)
