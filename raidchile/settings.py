f70d93c9002322599e277f847eee7016496dc3cc
Rafael V <ravb2013@gmail.com>
2019-04-07 23:40:01 -0400

    Create cart class inside cart app
    - Creating a cart class to manage all the tours reservations.
    - Editing the settings to work well with the newly created cart class.

--- raidchile/settings.py
+++ raidchile/settings.py
@@ -150,3 +150,6 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# Necessary settings for shopping cart by session
+CART_SESSION_ID = ‘cart’
