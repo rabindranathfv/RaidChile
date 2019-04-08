a1d99808230bdeb2b9ff762418804109149d29f7
Rafael V <ravb2013@gmail.com>
2019-04-07 22:41:29 -0400

    Create shopping cart app
    - Creating a shopping cart app with a dummy view.
    - Adding the new app to the installed apps and including it's urls into
    the project's urls.

--- raidchile/settings.py
+++ raidchile/settings.py
@@ -48,6 +48,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'raidchileapp',
     'contact',
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
