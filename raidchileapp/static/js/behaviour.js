f59581b40c557a5d01f17b7ae2681e1ab2a2a689
Rafael V <ravb2013@gmail.com>
2019-04-11 22:28:28 -0400

    Fix issues in the home template frontend
    - Making the cards of combos in the homepage have fixed height for their
    images with responsive breakpoints, thir descriptions are also height fixed
    with the property overflow-y set to scroll.
    - Centering everything in the previously mentioned cards.

--- raidchileapp/static/js/behaviour.js
+++ raidchileapp/static/js/behaviour.js
@@ -12,30 +12,4 @@ $( document ).ready(function(){
 	$("button.top-btns").click(function(){
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 	});
-
-	/*
-	var maxHeight = 0;
-
-	console.log("Firing resizing!")
-	$("div.w3-col div.w3-card-2, div.w3-half div.w3-card-2").each(function() {
-		if ($(this).height() > maxHeight)
-		{
-			maxHeight = $(this).height();
-		}
-	});
-	//set all specific classes to highest value from previous loop variable
-	$("div.w3-col div.w3-card-2, div.w3-half div.w3-card-2").each(function() {
-		$(this).css("height", maxHeight);
-	});*/
-
-	// Method to remove empty fields from GET forms.
-	/*
-	$("#search-form").submit(function() {
-		$(this).find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
-		return true; // Ensure form still submits
-	});*/
-
-	// Un-disable form fields when page loads, in case they click back after submission
-	//$("#search-form").find(":input").removeAttr("disabled");
-
 });
