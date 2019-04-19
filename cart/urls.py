52ce3128390727be81a770745bac9492e352ca32
Rafael V <ravb2013@gmail.com>
2019-04-19 12:50:30 -0400

    Refactor tour-combo-cart interactions
    - Refactoring cart implementation to utilize the new (Product)combo and tour models.
    - Redesigning the cart details and combo details templates.
    - Now only one combo OR multiple single tours can be in the cart at the same time.
    Otherwise you just get a warning telling you how the system works.

--- cart/urls.py
+++ cart/urls.py
@@ -7,6 +7,6 @@ app_name = 'cart'
 urlpatterns = [
 	path('', views.cart_detail, name='cart_detail'),
 	path('add/<int:product_id>/', views.cart_add, name='cart_add'),
-	path('add/combo/<slug:combo_slug>/', views.cart_add_combo, name='cart_add_combo'),
+	path('add/combo/<int:product_id>/', views.cart_add_combo, name='cart_add_combo'),
 	path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
