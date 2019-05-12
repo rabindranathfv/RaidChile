96b59013b385359be60b82b420351c1bb2ba971e
Rafael V <ravb2013@gmail.com>
2019-05-11 22:15:46 -0400

    Add production settings default values
    - Adding more urls to the allowed hosts.
    - Adding Django recomended security settings.
    - Adding a postgres DB setting for production environ.
    - Changing the static and media root settings for a dynamic value.
    - Adding loggin capabilities, even one to send to the site admins an
    email when there is an error or a critical error.
    - Updating requirements.txt
    - Removing the global 'static' folder from the git repository, so a
    collectstatic command execution is now mandatory.

--- raidchile/settings.py
+++ raidchile/settings.py
@@ -43,8 +43,19 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
                 'rafaelv.pythonanywhere.com',
+                'chileraid.cl',
+                'www.chileraid.cl',
                 'localhost'
                 ]
+
+# Security Settings #WARNING: Very important for production environments!!!
+
+SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', cast=int)
+SECURE_CONTENT_TYPE_NOSNIFF = config('SECURE_CONTENT_TYPE_NOSNIFF', cast=bool)
+SECURE_BROWSER_XSS_FILTER = config('SECURE_BROWSER_XSS_FILTER', cast=bool)
+SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', cast=bool)
+SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE', cast=bool)
+X_FRAME_OPTIONS = config('X_FRAME_OPTIONS')
 
 
 # Application definition
@@ -101,12 +112,24 @@ WSGI_APPLICATION = 'raidchile.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG: ## Using sqlite in development and another db engine in production.
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': config('DB_ENGINE'),
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT', cast=int),
+        }
+    }
 
 
 # Password validation
@@ -152,14 +175,14 @@ LANGUAGE_COOKIE_NAME = 'ChileRaid_language'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 #PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = config('STATIC_ROOT_PATH') or os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
 
 # User uploaded media directories and urls
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = config('MEDIA_ROOT_PATH') or os.path.join(BASE_DIR, 'media')
 
 
 # Email configuration
@@ -168,6 +191,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 # Necessary settings for shopping cart by session
 CART_SESSION_ID = 'cart'
@@ -176,19 +200,44 @@ CART_SESSION_ID = 'cart'
 RECAPTCHA_SECRET_KEY = config('RECAPTCHA_SECRET_KEY')
 RECAPTCHA_WEBSITE_KEY = config('RECAPTCHA_WEBSITE_KEY')
 
-# Logging settings show queries on the console for now.
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django.db.backends': {
-#             'level': 'DEBUG',
-#             'handlers': ['console', ],
-#         },
-#     },
-# }
+# Logging settings
+# These are the admins that will receive a detailed email in case  of any
+# 500 code error with the server.
+ADMINS = [
+    (config('ADMIN_USER1'),config('ADMIN_EMAIL1')),
+    (config('ADMIN_USER2'),config('ADMIN_EMAIL2'))
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s [%(asctime)s] %(module)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'maxBytes': 1024000,
+            'backupCount': 3,
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console', 'mail_admins'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
